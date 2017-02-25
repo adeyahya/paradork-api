772a2b74afbcfe927dd51f070355521d9eb8324a
Ade Yahya <adeyahyaprasetyo@gmail.com>
2017-02-26 00:01:33 +0700

    fix Travis CI deployment

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -5,11 +5,15 @@ set -xe
 if [ $TRAVIS_BRANCH == "master" ] ; then
 
     # setup ssh agent, git config and remote
-    eval "$(ssh-agent -s)"
-    ssh-add ~/.ssh/travis_rsa
-    git remote add deploy "travis@paradork.com:/var/www/paradork-api"
-    git config user.name "Travis CI"
-    git config user.email "travis@paradork.com"
+    openssl aes-256-cbc -K $encrypted_ff4d4c896f3c_key -iv $encrypted_ff4d4c896f3c_iv -in travis_rsa.enc -out travis_rsa -d
+    rm travis_rsa.enc
+    chmod 600 travis_rsa
+    mv travis_rsa ~/.ssh/id_rsa
+    # eval "$(ssh-agent -s)"
+    # ssh-add ~/.ssh/travis_rsa
+    # git remote add deploy "travis@paradork.com:/var/www/paradork-api"
+    # git config user.name "Travis CI"
+    # git config user.email "travis@paradork.com"
 
     # commit compressed files and push it to remote
     rm -f .gitignore
