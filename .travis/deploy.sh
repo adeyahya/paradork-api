2ed980eaebe2a1d0bd7ac2afeebc93f3f417e208
Ade Yahya <adeyahyaprasetyo@gmail.com>
2017-02-26 00:06:22 +0700

    fix Travis CI deployment

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,26 +2,23 @@
 # print outputs and exit on first failure
 set -xe
 
+# setup ssh agent, git config and remote
+openssl aes-256-cbc -K $encrypted_ff4d4c896f3c_key -iv $encrypted_ff4d4c896f3c_iv -in travis_rsa.enc -out travis_rsa -d
+rm travis_rsa.enc
+chmod 600 travis_rsa
+mv travis_rsa ~/.ssh/id_rsa
+
 if [ $TRAVIS_BRANCH == "master" ] ; then
-
-    # setup ssh agent, git config and remote
-    openssl aes-256-cbc -K $encrypted_ff4d4c896f3c_key -iv $encrypted_ff4d4c896f3c_iv -in travis_rsa.enc -out travis_rsa -d
-    rm travis_rsa.enc
-    chmod 600 travis_rsa
-    mv travis_rsa ~/.ssh/id_rsa
-    # eval "$(ssh-agent -s)"
-    # ssh-add ~/.ssh/travis_rsa
-    # git remote add deploy "travis@paradork.com:/var/www/paradork-api"
-    # git config user.name "Travis CI"
-    # git config user.email "travis@paradork.com"
-
-    # commit compressed files and push it to remote
-    rm -f .gitignore
-    cp .travis/deployignore .gitignore
+    cd html
+    git init
+        
+    git remote add deploy "deploy@kjaermaxi.me:/var/www/kjaermaxi.me"
+    git config user.name "Travis CI"
+    git config user.email "adeyahyaprasetyo@gmail.com"
+    
     git add .
-    git status # debug
-    git commit -m "Deploy compressed files"
-    git push -f deploy HEAD:master
+    git commit -m "Deploy"
+    git push --force deploy master
 else
     echo "No deploy script for branch '$TRAVIS_BRANCH'"
 fi
