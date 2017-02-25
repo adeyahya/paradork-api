4cd570332b2225a191d2b150a7f4dfa6d669fd3a
Ade Yahya <adeyahyaprasetyo@gmail.com>
2017-02-25 23:50:15 +0700

    fix Travis CI deployment

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -6,7 +6,7 @@ if [ $TRAVIS_BRANCH == "master" ] ; then
 
     # setup ssh agent, git config and remote
     eval "$(ssh-agent -s)"
-    # ssh-add ~/.ssh/travis_rsa
+    ssh-add ~/.ssh/travis_rsa
     git remote add deploy "travis@paradork.com:/var/www/paradork-api"
     git config user.name "Travis CI"
     git config user.email "travis@paradork.com"
